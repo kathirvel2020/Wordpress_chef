e688360670e5eb4585226905eb38ec0d289c2230
kathirvel2020 <33827020+kathirvel2020@users.noreply.github.com>
2018-02-12 05:55:59 +0530

    elb update link

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -14,10 +14,10 @@
  * @package WordPress
  */
 
-define('WP_REDIS_HOST', '');
+/**define('WP_REDIS_HOST', '');**//
  
-define('WP_HOME','');
-define('WP_SITEURL','');
+define('WP_HOME','wordpress-1801507371.us-east-1.elb.amazonaws.com');
+define('WP_SITEURL','wordpress-1801507371.us-east-1.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
